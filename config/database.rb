71e86f1aa57d3c054e8709062d28dec90c75a9b2
bohdan <korolevych.bogdan@gamil.com>
2017-03-01 20:00:53 +0200

    fix heroku bug

--- config/database.rb
+++ config/database.rb
@@ -24,7 +24,7 @@ ActiveRecord::Base.configurations[:development] = {
 
 ActiveRecord::Base.configurations[:production] = {
   adapter: 'postgresql',
-  host: 'still-anchorage-70952.herokuapp.com',
+  host: 'localhost',
   port: 5432,
   username: 'bogdan',
   password: '123456',
